9f3e019f73ab937609fcc1e9e7b5fb05ff9238e5
siddharth ravichandran <sid.ravichandran@gmail.com>
2018-05-04 10:26:23 +0530

    Removed sensitive account info

--- kube_files/kube_commands.sh
+++ kube_files/kube_commands.sh
@@ -21,6 +21,10 @@ kubectl create -f kube-deploy/chomeshvan-deployments.yml
 #lets say you make a change to the version number and then.
 kubectl apply -f chomeshvan-deployment.yml
 
+kubectl create -f kube-deploy/chomeshvan-deployments.yml
+#Command line updating an image and deploying via rolling deploys by changing the version number. 
+kubectl set image deployment/web chomeshvan-node=supersid/chomeshvan:1.0
+
 #Delete a node from Kubernetes 
 kubectl drain <node name> --delete-local-data --force --ignore-daemonsets
 kubectl delete node <node name>
--- pulling_image_from_a_private_dockerhub_repo.sh
+++ pulling_image_from_a_private_dockerhub_repo.sh
@@ -2,8 +2,8 @@
 
 kubectl create secret docker-registry myregistrykey \
 > --docker-server=https://index.docker.io/v1/ \
-> --docker-username=supersid \
-> --docker-password=theholygrail \
-> --docker-email=sid.ravichandran@gmail.com
+> --docker-username=dockerhubusername \
+> --docker-password=dockerhubpassword \
+> --docker-email=youremailn@gmail.com
 
 secret "myregistrykey" created
